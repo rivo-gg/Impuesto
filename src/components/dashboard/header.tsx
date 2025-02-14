092467ed984eaf45d65fbe99b89dc36cdba30f40
Taqib <taqib.ibrahim@gmail.com>
2025-02-14 17:35:50 +0100

    sticky header

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -11,7 +11,7 @@ const user = {
 
 function Header() {
   return (
-    <header className="bg-sidebar flex h-16 shrink-0 items-center gap-2 border-b border-dashed pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="bg-sidebar sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b border-dashed pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="hover:bg-sidebar-accent -ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
